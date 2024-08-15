1c8d02923c93deb153df9fcadd3e2ff97e2abcc2
Ethan Mullally <124309800+EthanMullally@users.noreply.github.com>
2024-08-15 17:59:29 -0400

    Update keyboard.asm

--- keyboard.asm
+++ keyboard.asm
@@ -2,32 +2,19 @@ ORG 0x8000
 BITS 16
 
 section .text
- global _start
+  global _checkForKeyPress
+  global _read 
 
-section .data
-    buffer: db 0
+section .data:
+  key: db "" 
 
-_start: 
-    .loop:
-        ; Check if a key is pressed
-        CMP AH, 01h 
-        JE .keyPressed
-        JC .loop
-        INT 0x16
-        
-    .keyPressed:
+_checkForKeyPress:
+   MOV AH, 00h
+   INT 0x16
+   JMP _read
 
-        ; Read the key press and store it into a buffer then output the buffer
-        MOV AH, 02h
-        INT 0x16
-        MOV [buffer], AL
-        MOV AL, [buffer]
-        JMP .output
-
-    .output:
-        ; Output the key pressed
-        MOV AH, 0x0E ; 0x0E is Teletype which handles ASCII which allows us to view each keypress as it is on the keyboard
-        MOV BH, 0x0  ; Page number
-        MOV BL, 0x07 ; White text black background
-        INT 0x10 
-        JMP .loop
+_read:11
+   EXTERN _displayKey
+   MOV AH, 01h
+   INT 0x16
+   JMP _displayKey
